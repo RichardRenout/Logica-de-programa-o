c85185d9c0ed8255eb91aea643c3e36d5ba509f0
RichardRenout <richardrenout@gmail.com>
2026-08-21 16:49:02 -0300

    Add files via upload

---
+++ 6 - antecessor_sucessor.py
@@ -0,0 +1,13 @@
+import os
+os.system('cls')
+
+print('\n SOLICITANDO DADOS')
+numero = int(input('Digite um numero'))
+
+#CALCULANDO
+antecessor = numero - 1
+sucessor = numero + 1
+
+print('\n EXIBINDO DADOS ')
+print('Antecessor: ', antecessor)
+print('Sucessor: ', sucessor)
